f937be27f7f43866a0f991368a29e3fc1b9bf299
rgobet <rgobet@student.42angouleme.fr>
2026-04-14 17:24:41 +0200

    get theta

--- predict.py
+++ predict.py
@@ -3,8 +3,15 @@ import csv
 import os
 
 def main():
-    retrieveTheta()
+    content = retrieveTheta()
 
 def retrieveTheta():
-    with os.open('./theta.predict', 'r') as file:
-        content = file.read()
+    if not os.path.exists('./theta.predict'):
+        return 0.0, 0.0
+    if not os.access('./theta.predict', os.R_OK):
+        exit(1)
+    with open('./theta.predict', 'r') as file:
+        theta = file.read().split('\n')
+        return float(theta[0]), float(theta[1])
+
+main()
