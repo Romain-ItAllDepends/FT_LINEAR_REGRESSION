46369afc9af6eb23f79b835777106b0a9d8f2644
rgobet <rgobet@student.42angouleme.fr>
2026-04-14 17:33:56 +0200

    predict program done

--- predict.py
+++ predict.py
@@ -3,7 +3,9 @@ import csv
 import os
 
 def main():
-    content = retrieveTheta()
+    theta = retrieveTheta()
+    mileage = input("What is the vehicle's mileage?\n")
+    estimatePrice(theta, mileage)
 
 def retrieveTheta():
     if not os.path.exists('./theta.predict'):
@@ -13,5 +15,9 @@ def retrieveTheta():
     with open('./theta.predict', 'r') as file:
         theta = file.read().split('\n')
         return float(theta[0]), float(theta[1])
+
+def estimatePrice(theta, mileage):
+    res = theta[0] + (theta[1] * float(mileage))
+    print(res)
 
 main()
