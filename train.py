af01df7dbdd916a5044f0732b0750916b04e3bda
rgobet <rgobet@student.42angouleme.fr>
2026-04-23 18:21:44 +0200

    normalize

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 import csv
 import os
 
+minMileage, minPrice = 0, 0
+maxMileage, maxPrice = 0, 0
+
 def main():
     lines = retrieveData()
-    theta = train(lines)
+    normedData = normalize(lines)
+    theta = train(normedData)
     saveTheta(theta)
 
 def retrieveData():
@@ -14,6 +18,7 @@ def retrieveData():
     with open('./data.csv', 'r') as file:
         lines = file.read().split('\n')
         values = [line.split(',') for line in lines if parseHeader(line)]
+        values = [[int(x), int(y)] for x, y in values]
     return values
 
 def saveTheta(theta):
@@ -22,11 +27,42 @@ def saveTheta(theta):
         file.write('\n')
         file.write(str(theta[1]))
 
+def minValues(list):
+    minMileage = list[0][0]
+    minPrice = list[0][1]
+    for i in range (0, len(list)):
+        if list[i][0] < minMileage:
+            minMileage = list[i][0]
+        if list[i][1] < minPrice:
+            minPrice = list[i][1]
+    return minMileage, minPrice
+
+def maxValues(list):
+    maxMileage = 0
+    maxPrice = 0
+    for i in range(0, len(list)):
+        if list[i][0] > maxMileage:
+            maxMileage = list[i][0]
+        if list[i][1] > maxPrice:
+            maxPrice = list[i][1]
+    return maxMileage, maxPrice
+
+def normalize(list):
+    global minMileage, maxMileage, minPrice, maxPrice
+    minMileage, minPrice = minValues(list)
+    maxMileage, maxPrice = maxValues(list)
+    print(minPrice, maxPrice, minMileage, maxMileage)
+    normedMileage = [(x[0] - minMileage) / (maxMileage - minMileage) for x in list]
+    normedPrice = [(x[1] - minPrice) / (maxPrice - minPrice) for x in list]
+    print("Normed Mileage: ", normedMileage)
+    print("\nNormed Price: ", normedPrice)
+    return normedMileage, normedPrice
+
 def train(lines):
     tmpTheta0, tmpTheta1 = getTheta()
     for i in range (0, 10000):
-        tmpTheta0 -= 0.001 * 1 / len(lines) * sum([estimatePrice(float(line[0]), tmpTheta0, tmpTheta1)- float(line[1]) for line in lines])
-        tmpTheta1 -= 0.001 * 1 / len(lines) * sum([(estimatePrice(float(line[0]), tmpTheta0, tmpTheta1) - float(line[1])) * float(line[0]) for line in lines])
+        tmpTheta0 -= 0.0000001 * 1 / len(lines) * sum([estimatePrice(float(line[0]), tmpTheta0, tmpTheta1)- float(line[1]) for line in lines])
+        tmpTheta1 -= 0.0000001 * 1 / len(lines) * sum([(estimatePrice(float(line[0]), tmpTheta0, tmpTheta1) - float(line[1])) * float(line[0]) for line in lines])
     return tmpTheta0, tmpTheta1
 
 def estimatePrice(mileage, theta0, theta1):
