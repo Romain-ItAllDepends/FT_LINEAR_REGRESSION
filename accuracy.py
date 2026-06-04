5c56983b638fcc6958273e3fe8538f72735ac7f1
unknown <Romain.Gobet30@hotmail.com>
2026-06-04 19:05:48 +0200

    Accuracy program

---
+++ accuracy.py
@@ -0,0 +1,62 @@
+import numpy as np
+import os
+
+def main():
+	data = retrieveData()
+	prediction = retrievePrediction()
+	evaluate(data, prediction)
+
+def evaluate(data, prediction):
+    dataDict = {m: p for m, p in data} # Creation of a dictionary (like map in c++)
+
+    totalError = 0
+    count = 0
+
+    for m, pricePred in prediction:
+        if m in dataDict:
+            priceData = dataDict[m]
+
+            totalError += abs(pricePred - priceData) / priceData
+            count += 1
+
+    accuracy = (1 - totalError / count) * 100
+
+    print("Accuracy:", accuracy, "%")
+
+def retrievePrediction():
+	if not os.path.exists('./result.predict'):
+		exit(1)
+	if not os.access('./result.predict', os.R_OK):
+		exit(1)
+	with open('./result.predict', 'r') as file:
+		lines = file.read().strip().split('\n')
+		file = parseFile(lines)
+		if not file:
+			print("The prediction file is empty or contains alphanumeric characters!")
+			exit(0)
+	return file
+
+def retrieveData():
+	if not os.path.exists('./data.csv'):
+		exit(1)
+	if not os.access('./data.csv', os.R_OK):
+		exit(1)
+	with open('./data.csv', 'r') as file:
+		lines = file.read().strip().split('\n')
+		file = parseFile(lines)
+		if not file:
+			print("The data file is empty or contains alphanumeric characters!")
+			exit(0)
+	return file
+
+def parseFile(file):
+	fileParsed = []
+	for line in file:
+		try:
+			x, y = line.split(',')
+			fileParsed.append((int(x), int(y)))
+		except:
+			continue
+	return fileParsed
+
+main()
